4715b2a2a167a238da2597671843566d7832653f
your name <you@exammple.com>
2025-08-04 22:26:19 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,16 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { SITE } from './src/config.mjs';
-
 export default defineConfig(() => {
-    return {
-        base: SITE.basePathname,
-        plugins: [
-            qwikCity({
-                trailingSlash: SITE.trailingSlash,
-            }),
-            qwikVite(),
-            tsconfigPaths(),
-        ],
-        preview: {
-            headers: {
-                'Cache-Control': 'public, max-age=600',
-            },
-        }
-    };
+  return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    optimizeDeps: {
+      include: ['@auth/qwik']
+    },
+    preview: {
+      headers: {
+        'Cache-Control': 'public, max-age=600',
+      },
+    },
+  };
 });
