62a92978877a1d267bbb1e2e210904d1d0505a5e
your name <you@exammple.com>
2025-08-21 11:18:56 -0400

    commit

--- src/root.tsx
+++ src/root.tsx
@@ -5,6 +5,7 @@ import styles from "~/assets/styles/global.css?inline";
 import { ObserverProvider } from "./components/common/ObserverProvider";
 import { APP_STATE_CONTEXT_ID } from "./components/widgets/AppStateContext";
 import { AppState } from "./components/widgets/AppStateType";
+import Header from "./components/widgets/Header";
 
 export default component$(() => {
   useStyles$(styles);
@@ -30,22 +31,23 @@ export default component$(() => {
         <ServiceWorkerRegister />
       </head>
       <body class="bg-white antialiased">
-        <div class="relative border-x mx-auto max-w-7xl overflow-x-hidden">
+        <Header /> {/* Render header directly in body */}
+        <div class="relative border-x mx-auto max-w-7xl overflow-x-hidden"> {/* Added overflow-x-hidden */}
           {/* Watercolor background with lighter green gradients and clay texture */}
           <div class="absolute inset-0 bg-watercolor-texture opacity-50 bg-secondary-100/50" aria-hidden="true"></div>
           <div class="absolute inset-0 bg-gradient-to-br from-primary-100/95 via-primary-200/80 to-tertiary-200/85" aria-hidden="true"></div>
           <div class="absolute inset-0 bg-gradient-to-t from-tertiary-300/40 via-primary-200/50 to-primary-100/60" aria-hidden="true"></div>
-          {/* Floating blurry balls constrained within max-w-7xl */}
-          <div class="absolute top-20 left-10 w-32 h-32 bg-tertiary-300/30 rounded-full blur-xl animate-float max-w-full" aria-hidden="true"></div>
-          <div class="absolute top-80 left-10 w-40 h-40 bg-tertiary-300/30 rounded-full blur-xl animate-float max-w-full" aria-hidden="true"></div>
-          <div class="absolute bottom-20 right-10 w-24 h-24 bg-secondary-200/25 rounded-full blur-xl animate-float max-w-full" style="animation-delay: -2s;" aria-hidden="true"></div>
-          <div class="absolute top-1/2 left-10 w-16 h-16 bg-primary-200/30 rounded-full blur-lg animate-float max-w-full" style="animation-delay: -4s;" aria-hidden="true"></div>
-          <div class="absolute top-1/3 right-10 w-20 h-20 bg-secondary-400/25 rounded-full blur-lg animate-float max-w-full" style="animation-delay: -1s;" aria-hidden="true"></div>
-          <div class="absolute bottom-1/4 left-10 w-28 h-28 bg-tertiary-300/25 rounded-full blur-xl animate-float max-w-full" style="animation-delay: -3s;" aria-hidden="true"></div>
-          <div class="absolute top-1/4 right-10 w-18 h-18 bg-secondary-300/30 rounded-full blur-lg animate-float max-w-full" style="animation-delay: -5s;" aria-hidden="true"></div>
-          <div class="absolute bottom-1/3 left-1/2 w-22 h-22 bg-primary-100/25 rounded-full blur-xl animate-float max-w-full" style="animation-delay: -0.5s;" aria-hidden="true"></div>
-          <div class="absolute top-10 right-10 w-26 h-26 bg-secondary-200/25 rounded-full blur-xl animate-float max-w-full" style="animation-delay: -6s;" aria-hidden="true"></div>
-          <div class="absolute bottom-10 left-10 w-20 h-20 bg-secondary-300/30 rounded-full blur-lg animate-float max-w-full" style="animation-delay: -7s;" aria-hidden="true"></div>
+          {/* Floating blurry balls with increased clay emphasis */}
+          <div class="absolute top-20 left-10 w-32 h-32 bg-tertiary-300/30 rounded-full blur-xl animate-float" aria-hidden="true"></div>
+          <div class="absolute top-80 left-80 w-40 h-40 bg-tertiary-300/30 rounded-full blur-xl animate-float" aria-hidden="true"></div>
+          <div class="absolute bottom-20 right-10 w-24 h-24 bg-secondary-200/25 rounded-full blur-xl animate-float" style="animation-delay: -2s;" aria-hidden="true"></div>
+          <div class="absolute top-1/2 left-1/4 w-16 h-16 bg-primary-200/30 rounded-full blur-lg animate-float" style="animation-delay: -4s;" aria-hidden="true"></div>
+          <div class="absolute top-1/3 right-1/4 w-20 h-20 bg-secondary-400/25 rounded-full blur-lg animate-float" style="animation-delay: -1s;" aria-hidden="true"></div>
+          <div class="absolute bottom-1/4 left-1/3 w-28 h-28 bg-tertiary-300/25 rounded-full blur-xl animate-float" style="animation-delay: -3s;" aria-hidden="true"></div>
+          <div class="absolute top-1/4 right-1/3 w-18 h-18 bg-secondary-300/30 rounded-full blur-lg animate-float" style="animation-delay: -5s;" aria-hidden="true"></div>
+          <div class="absolute bottom-1/3 left-1/2 w-22 h-22 bg-primary-100/25 rounded-full blur-xl animate-float" style="animation-delay: -0.5s;" aria-hidden="true"></div>
+          <div class="absolute top-10 right-20 w-26 h-26 bg-secondary-200/25 rounded-full blur-xl animate-float" style="animation-delay: -6s;" aria-hidden="true"></div>
+          <div class="absolute bottom-10 left-20 w-20 h-20 bg-secondary-300/30 rounded-full blur-lg animate-float" style="animation-delay: -7s;" aria-hidden="true"></div>
 
           <ObserverProvider>
             <RouterOutlet />
