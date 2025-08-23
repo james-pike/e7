2529734b9b1d0d04aa6e30ae67aae65b372187c2
your name <you@exammple.com>
2025-08-23 11:54:21 -0400

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -3,8 +3,6 @@ import { useContent, useLocation } from "@builder.io/qwik-city";
 import IconChevronDown from "../icons/IconChevronDown";
 import MenuModal from "./MenuModal";
 
-//
-
 export default component$(() => {
   const store = useStore({
     isScrolling: false,
@@ -29,17 +27,15 @@ export default component$(() => {
         }
       }}
     >
-
-      
       <div class="absolute inset-0" aria-hidden="true"></div>
-      <div class="relative text-default py-2 md:p-1 pr-2 -pl-0 md:pr-6 md:pl-2 mx-auto w-full md:flex md:justify-between max-w-7xl">
+      <div class="relative text-default py-2 md:p-1 px-2 md:px-6 mx-auto w-full md:flex md:justify-between max-w-7xl">
         <div class="mr-auto rtl:mr-0 rtl:ml-auto flex justify-between">
           <a class="flex flex-col items-center" href="/">
-            <div class="w-[100px] h-[40px] md:w-[150px] md:h-[60px] mt-1">
+            <div class="w-[100px] h-[40px] md:w-[150px] md:h-[60px] pt-1">
               <img src="/images/logo2.svg" alt="Logo" class="w-full h-full object-contain" />
             </div>
             <span
-              class="text-secondary-800 font-semibold text-[5px] sm:hidden md:block md:text-[8px] -mt-2"
+              class="hidden md:block text-secondary-800 font-semibold md:text-[8px] mt-0.5"
             >
               Listening, Connecting, Creating
             </span>
