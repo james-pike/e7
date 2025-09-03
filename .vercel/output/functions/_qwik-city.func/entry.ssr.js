4e69ec98f9e70580cbb025805b523cd4cc54775a
your name <you@exammple.com>
2025-09-03 14:03:43 -0400

    commit

--- .vercel/output/functions/_qwik-city.func/entry.ssr.js
+++ .vercel/output/functions/_qwik-city.func/entry.ssr.js
@@ -1,1 +1,1 @@
-import"./q-eDAWdLfM.js";import{r as a}from"./q-Bi8TJw7F.js";export{a as default};
+import"./q-eDAWdLfM.js";import{r as a}from"./q-DDNzClQ5.js";export{a as default};
--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -30,7 +30,7 @@ export default component$(() => {
               </span>
             </h1>
             {/* Slogan */}
-            <h2 class="!text-3xl  md:!text-3xl xdxd font-bold -mx-2 text-secondary-800 md:text-primary-600 mb-4 md:mb-8 mt-5 ">
+            <h2 class="!text-3xl  md:!text-3xl xdxd font-bold -mx-2 text-secondary-800 md:text-primary-600 mb-4 md:mb-8 mt-6 ">
               <span class="bg-gradient-to-r from-primary-600 via-tertiary-600 to-primary-600 bg-clip-text text-transparent">
               Listening, Connecting & Creating
               </span>
